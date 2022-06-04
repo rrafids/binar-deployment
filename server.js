8899b1c36e5ba60f087071d21bb2b6449a64cc7f
Raihan Rafid <raihanrfd1@gmail.com>
2022-06-04 09:14:03 +0700

    fix port

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Public File Access
 app.use("/public/files", express.static(path.join(__dirname, "/storages")));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server berhasil berjalan di port http://localhost:${PORT}`);
 });
